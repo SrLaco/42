e25e58f401ef8eb28bbe715f7e384f96226e6809
SrLaco <car.edu.mou.ara@gmail.com>
2026-06-25 17:34:09 -0300

    split refactor

--- M_0/libft/ft_split.c
+++ M_0/libft/ft_split.c
@@ -5,14 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cade-mou <cade-mou@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/24 22:59:54 by cade-mou          #+#    #+#             */
-/*   Updated: 2026/06/25 03:51:12 by cade-mou         ###   ########.fr       */
+/*   Created: 2026/06/25 09:38:43 by cade-mou          #+#    #+#             */
+/*   Updated: 2026/06/25 09:38:43 by cade-mou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	word_count(char const *str, char c)
+static size_t	word_count(const char *str, char c)
 {
 	size_t	i;
 	size_t	j;
@@ -21,7 +21,7 @@ static size_t	word_count(char const *str, char c)
 	j = 0;
 	while (str[i] == '\0')
 	{
-		if ((str[i] != c && str[i - 1] == c) && i > 0)
+		if((str[i] != c && str[i - 1] == c) && i > 0)
 			j++;
 		i++;
 	}
@@ -35,43 +35,42 @@ static char	*set_next_line(const char **str, char c)
 
 	while (**str == c)
 		(*str)++;
-	if (**str == '\0')
+	if (**str == '/0')
 		return (NULL);
-	word_len = ft_strchr(*str, (int)c);
+	word_len = 
 	current_word = ft_substr(*str, 0, word_len);
-	if (!current_word)
-		return (NULL);
-	while (**str != c && **str != '\0')
-		(*str)++;
-	return (current_word);
+	if (current_word)
+	{
+		while (**str != c && **str !=0)
+			(*str)++;
+	}
+	return (NULL);
 }
-
-static void	ft_free(char **split, int i)
+static void	chain_free(char **, int i)
 {
-	while (i >= 0)
+	while (i > 0)
 	{
 		i--;
 		free(split[i]);
 	}
-	return ;
 }
 
-char	**ft_split(char const *str, char c)
+char	**(const char *str, char c)
 {
 	char	**split;
 	size_t	w_count;
 	size_t	i;
 
-	w_count = word_count(str, c);
-	split = malloc((1 + w_count) * sizeof(char *));
+	w_count = word_count (str, c);
+	split = (malloc(w_count + 1) * sizeof(char *))
 	if (split == NULL)
 		return (NULL);
 	i = 0;
-	while (i < w_count)
+	while i (i < w_count)
 	{
 		split[i] = set_next_line(&str, c);
 		if (!split[i])
-			ft_free(split, i);
+			chain_free(split, i);
 		i++;
 	}
 	split[i] = NULL;
