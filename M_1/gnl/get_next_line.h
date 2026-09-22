cdf8583aede4bf648832a4f394783d358dbc6b2a
SrLaco <car.edu.mou.ara@gmail.com>
2026-09-22 04:08:19 -0300

    gnl v1 pending readme

---
+++ M_1/gnl/get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cade-mou <cade-mou@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/22 04:05:42 by cade-mou          #+#    #+#             */
+/*   Updated: 2026/09/22 04:05:42 by cade-mou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
